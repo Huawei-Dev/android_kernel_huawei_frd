930aa4ffc9f1fc8dbec1e5b02ce2f9a08eb59ebf
Daniel Rosenberg <drosen@google.com>
2016-03-23 16:39:30 -0700

    sdcardfs: remove effectless config option CONFIG_SDCARD_FS_CI_SEARCH only guards a define for LOOKUP_CASE_INSENSITIVE, which is never used in the kernel. Remove both, along with the option matching that supports it.
    Change-Id: I363a8f31de8ee7a7a934d75300cc9ba8176e2edf
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    (cherry picked from commit d4ecd64643a034f9445d6c7c54d247235200098a)

--- fs/f2fs/namei.c
+++ fs/f2fs/namei.c
@@ -256,7 +256,7 @@ static struct dentry *f2fs_lookup(struct inode *dir, struct dentry *dentry,
 	if (dentry->d_name.len > F2FS_NAME_LEN)
 		return ERR_PTR(-ENAMETOOLONG);
 
-	if (flags & LOOKUP_CASE_INSENSITIVE)
+	if (flags)
 		de = f2fs_find_entry(dir, &dentry->d_name, &page, CASE_INSENSITIVE);
 	else
 		de = f2fs_find_entry(dir, &dentry->d_name, &page, CASE_SENSITIVE);
